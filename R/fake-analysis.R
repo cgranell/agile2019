8a7675df80fc7aef58ba49182208dc033a53af36
Carlos Granell <canut@uji.es>
2019-06-13 09:08:33 +0200

    files reorganisation

--- R/fake-analysis.R
+++ R/fake-analysis.R
@@ -8,7 +8,7 @@ url <- "https://fme.discomap.eea.europa.eu/fmedatastreaming/AirQualityDownload/A
 
 library(here)
 
-data_aq <- here("R", "data")
+data_aq <- here("R", "data", "aq")
 
 files = list.files(data_aq, pattern = "*.csv", full.names = TRUE)
 r = lapply(files, function(f) read.csv(f, encoding = "UTF-8"))
@@ -34,7 +34,7 @@ sel = apply(aq, 2, function(x) sum(is.na(x)) < 0.75 * 365 * 24)
 aqsel = aq[, sel] # stations are in columns
 
 library(tidyverse)
-read.csv(here("R", "AirBase_v8_stations.csv"), sep = "\t", stringsAsFactors = FALSE) %>% 
+read.csv(here("R", "data", "AirBase_v8_stations.csv"), sep = "\t", stringsAsFactors = FALSE) %>% 
   as_tibble  %>% 
   # filter(country_iso_code == "CY", type_of_station == "Background") -> a2
   filter(country_iso_code == "CY") -> a2
@@ -53,19 +53,18 @@ right_join(a2.sf, tb) %>% st_transform(crs) -> no2.sf
 # load Cyprus boundaries
 library(cshapes)
 cshp = cshp(as.Date("2000-01-1"))
-cy = cshp[cshp$ISO1AL2 == "CY",]
-cy <- st_transform(st_as_sf(cy), crs)
+cy = cshp[cshp$ISONAME == "Cyprus",]
+cy.sf <- st_transform(st_as_sf(cy), crs)
 
-ggplot() + geom_sf(data=cy) + geom_sf(data = no2.sf, mapping = aes(col = NO2))
-
-
+ggplot() + geom_sf(data=cy.sf) + geom_sf(data = no2.sf, mapping = aes(col = NO2)) + 
+  geom_sf_text(data = no2.sf, aes(label = station_local_code), colour = "black", size=2, nudge_y = -1)
 
 ## some text plus R expression
 
 
 library(gstat)
-# build a grid over Germany:
-bb = st_bbox(cy)
+# build a grid over Cyprus:
+bb = st_bbox(cy.sf)
 dx = seq(bb[1], bb[3], 10000)
 dy = seq(bb[4], bb[2], -10000) # decreases!
 st_as_stars(matrix(0, length(dx), length(dy))) %>%
@@ -73,13 +72,13 @@ st_as_stars(matrix(0, length(dx), length(dy))) %>%
   st_set_dimensions(2, dy) %>%
   st_set_dimensions(names = c("x", "y")) %>%
   st_set_crs(crs) -> grd
-i = st_intersects(grd, cy)
+i = st_intersects(grd, cy.sf)
 grd[[1]][lengths(i)==0] = NA
 grd
 
 v <- gstat::variogram(NO2~1, no2.sf)
 plot(v, plot.numbers = TRUE)
-v.m = gstat::fit.variogram(v, vgm(1, "Exp", 50000, 1))
+v.m = gstat::fit.variogram(v, gstat::vgm(1, "Exp", 50000, 1))
 k = gstat::krige(NO2~1, no2.sf, grd, v.m)
 #> [using ordinary kriging]
 ggplot() + geom_stars(data = k, aes(fill = var1.pred, x = x, y = y)) + 
